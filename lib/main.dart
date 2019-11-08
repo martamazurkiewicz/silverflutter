4d5c75da5de77c1ec88679bd79c9b45a2b6fac26
martamazurkiewicz <marta@mazurkiewi.cz>
2019-11-08 15:58:32 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,39 +18,38 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding( 
-                  padding: EdgeInsets.all(5),
-                  child:
-                  Container(
-                  color: Colors.grey,
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Text("tekst1"),
-                              Text(".Net", style: TextStyle(color: Colors.red))
-                            ],
-                          ),
-                          Text(
-                            "tekst2",
-                            style: TextStyle(fontSize: 30, color: Colors.red),
-                          )
-                        ],
-                      ),
-                      Image.asset("assets/silver.png"),
-                      RaisedButton(
-                        onPressed: () {},
-                        color: Colors.red,
-                        child: Text("Buttton, ktory nic nie robi"),
-                      )
-                    ],
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Container(
+                    color: Colors.grey,
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Text("tekst1"),
+                                Text(".Net",
+                                    style: TextStyle(color: Colors.red))
+                              ],
+                            ),
+                            Text(
+                              "tekst2",
+                              style: TextStyle(fontSize: 30, color: Colors.red),
+                            )
+                          ],
+                        ),
+                        Image.asset("assets/silver.png"),
+                        RaisedButton(
+                          onPressed: () {},
+                          color: Colors.red,
+                          child: Text("Buttton, ktory nic nie robi"),
+                        )
+                      ],
+                    ),
                   ),
-                ),
                 )
-                
               ],
             ),
           ),
